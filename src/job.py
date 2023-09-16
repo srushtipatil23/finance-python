3764f7726bbc6438153997e439a717696fd55f4e
Pradeep Pai Kateel <pradeeppaikateel@gmail.com>
2023-09-16 22:08:47 +0530

    Created a placeholder file for coding

---
+++ src/job.py
@@ -0,0 +1,4 @@
+# Write a code to read the file from input folder with the file name as a
+# dataframe and write the file into a new folder called as output in root without using pandas for the txt file
+
+print("Hi")
